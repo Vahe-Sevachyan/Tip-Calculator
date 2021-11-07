1a4f06340b1d3b7f26e9707bf07ddd631656ed9e
Vahe Sevachyan <vscode13@gmail.com>
2021-11-06 22:50:50 -0700

    Updates to JS

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const tipInput = document.querySelector(".tipInput");
+// const billAmount = document.querySelector(".billAmount");
 const peopleAmount = document.querySelector(".peopleAmount");
 const tipAmount = document.querySelector(".tipAmount5");
+const tipPerEach = document.querySelector(".tipPerPerson");
 
 const calculateTip5 = () => {
-  const tipValue = tipInput.value;
-  const peopleCount = peopleAmount.value;
-  const total = tipValue * peopleCount * 0.5;
-  console.log(total);
+  const tipPercent = 0.05;
+  //bill total
+  const billAmountValue = document.querySelector(".billAmount").value;
+  //tip amount
+  const tipAmount = billAmountValue * tipPercent;
+  //tip amount per person
+  const tipPerPerson = tipAmount / peopleAmount;
+  //bill per person
+  const billPerPerson = billAmount + tipAmount / peopleAmount;
+  //
+  tipPerEach.innerHTML = tipPerPerson;
+  //console.log(total);
 };
 
 tipAmount.addEventListener("click", calculateTip5);
